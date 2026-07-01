b3987f5c443cb6f7152c4e1176ec8040ac86cc0f
Dharamveer Vadoliya <dharamveervadoliya@gmail.com>
2026-07-01 17:09:40 +0530

    add class and objects

---
+++ demo.py
@@ -0,0 +1,31 @@
+class Car:
+    
+#     def __init__(self , id , name):
+#         self.id = id
+#         self.name = name
+    
+#     def display(self):
+#         print(self.id)
+#         print(self.name)
+
+# a = Car("123", "fghdfhgdf")
+# a.display()
+
+        
+    def dataput(self):
+        self.id = int(input("Enter a id :"))
+        self.name = input("Enter a name :")
+        self.salary = float(input("Enter a salary:"))
+
+    def display(self):
+        print(self.id)
+        print(self.name)
+        print(self.salary)
+
+obj1 = Car()
+obj1.dataput()
+obj1.display()
+
+
+
+
--- test.py
+++
@@ -1,4 +0,0 @@
-print("Hello, World!")
-
-for i in range(5):
-    print(f"This is iteration {i + 1}")
